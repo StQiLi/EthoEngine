a0348316d86ee49a2a366ac3499402c18bfbb0ce
StQiLi <stevenqingli@gmail.com>
2024-05-18 16:59:55 -0400

    some react stuff

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { withAuthInfo, useRedirectFunctions, useLogoutFunction } from '@propelauth/react';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = withAuthInfo((props) => {
+    const logoutFunction = useLogoutFunction();
+    const { redirectToLoginPage, redirectToSignupPage, redirectToAccountPage } = useRedirectFunctions();
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    if (props.isLoggedIn) {
+        return (
+            <div>
+                <p>You are logged in as {props.user.email}</p>
+                <button onClick={() => redirectToAccountPage()}>Account</button>
+                <button onClick={() => logoutFunction(true)}>Logout</button>
+            </div>
+        );
+    } else {
+        return (
+            <div>
+                <p>You are not logged in</p>
+                <button onClick={() => redirectToLoginPage()}>Login</button>
+                <button onClick={() => redirectToSignupPage()}>Signup</button>
+            </div>
+        );
+    }
+});
 
-export default App
+export default App;
--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { AuthProvider } from '@propelauth/react';
+import App from './App';
+
+const authUrl = "https://3554294.propelauthtest.com";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <AuthProvider authUrl={authUrl}>
+      <Router>
+        <App />
+      </Router>
+    </AuthProvider>
+  </React.StrictMode>
+);
